81422ed0d7f8679514645a2646f632c97371591b
Ken Sodemann <ken@ionic.io>
2021-02-13 19:16:09 -0600

    unit tests

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
@@ -7,18 +8,27 @@ describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [HomePage],
+        imports: [IonicModule],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(HomePage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    }),
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('displays the correct title', () => {
+    const titles = fixture.debugElement.queryAll(By.css('ion-title'));
+    expect(titles.length).toBe(2);
+    expect(titles[0].nativeElement.textContent.trim()).toBe('Blank');
+    expect(titles[1].nativeElement.textContent.trim()).toBe('Blank');
   });
 });
